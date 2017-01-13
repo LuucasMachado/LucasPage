162e202422f7e4d81129348f03f128ceab844f62
Lucas Machado <lucashenrique.2d@gmail.com>
2017-01-13 12:39:43 -0200

    Iniciando

---
+++ src/components/Body/Contact/Contact.js
@@ -0,0 +1,19 @@
+// @flow
+'use strict';
+
+import React, { Component } from 'react';
+import './Contact.styl';
+import style from './style';
+
+
+class Contact extends Component {
+	render(){
+		return(
+			<div className="Contact">
+
+			</div>
+		)
+	}
+}
+
+export default Contact;
